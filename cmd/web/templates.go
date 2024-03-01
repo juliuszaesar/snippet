86d846cd4da278eb91f92bcf8c07a8fc21808c22
juliusnaski <julius.naski@forefront.se>
2024-03-01 09:04:59 +0100

    added new helper function and humanDate for parsing dateTime objects

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,11 +4,21 @@ import (
 	"html/template"
 	"letsgo/internal/models"
 	"path/filepath"
+	"time"
 )
 
 type templateData struct {
-	Snippet  models.Snippet
-	Snippets []models.Snippet
+	Snippet     models.Snippet
+	Snippets    []models.Snippet
+	CurrentYear int
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("2 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -22,8 +32,11 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		// Parse the base template file into a template set.
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		// The template.FuncMap must be registered with the template set before you
+		// call the ParseFiles() method. This means we have to use template.New() to
+		// create an empty template set, use the Funcs() method to register the
+		// template.FuncMap, and then parse the file as normal.
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
